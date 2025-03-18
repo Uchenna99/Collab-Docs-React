2f2ab914d77df260825c59cae022210b81d99c84
Uchenna99 <uchennaagbu@gmail.com>
2025-03-18 22:37:55 +0100

    started edit page navbar

--- src/pages/DocumentPage.tsx
+++ src/pages/DocumentPage.tsx
@@ -5,15 +5,27 @@ const DocumentPage = () => {
     <>
       <div className="document-page-wrap">
         <div className="docs-navbar">
+
           <div className="docs-navbar-left">
             <div className="docs-navbar-left-img"></div>
+
             <div className="docs-navbar-left-container">
-              <div className="container-top"></div>
+
+              <div className="container-top">
+                <div className="doc-name">
+                  <p>Untitled document</p>
+                </div>
+                <div className="online-status"></div>
+              </div>
+
               <div className="container-bottom"></div>
+
             </div>
+
           </div>
 
           <div className="docs-navbar-right"></div>
+
         </div>
 
         <div className="menu-bar"></div>

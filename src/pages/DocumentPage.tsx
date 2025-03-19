200260bf229307ce375dc2da6a21db2fd756200f
Uchenna99 <uchennaagbu@gmail.com>
2025-03-19 22:12:02 +0100

    figured out the svgs

--- src/pages/DocumentPage.tsx
+++ src/pages/DocumentPage.tsx
@@ -2,6 +2,8 @@ import { LuHistory } from "react-icons/lu";
 import { MdOutlineComment } from "react-icons/md";
 import { BiVideo } from "react-icons/bi";
 import { MdArrowDropDown } from "react-icons/md";
+import { MdOutlineSearch } from "react-icons/md";
+import svgIcons from "../assets/material_common_sprite794_gm3_grey_medium.svg"
 
 const DocumentPage = () => {
   return (
@@ -80,16 +82,57 @@ const DocumentPage = () => {
           <div className="menu-bar">
 
             <div className="menu-group-one">
+
               <div className="menu-option-div">
+                <MdOutlineSearch id="menu-icons" />
               </div>
+
+              <div className="menu-option-div">
+                <div className="icon-holder">
+                  <svg  viewBox="130 754 1938 1224" height="1224">
+                    <path fill="#444746" d="M136 770v-1.5h6q1.042 0 1.771-.729T144.5 766t-.729-1.771T142 763.5h-5.125l1.937 1.938-1.062 1.062-3.75-3.75 3.75-3.75 1.062 1.062-1.937 1.938H142q1.667 0 2.833 1.167Q146 764.333 146 766t-1.167 2.833Q143.667 770 142 770Z"/>
+                  </svg>
+                </div>
+              </div>
+
+              <div className="menu-option-div">
+                <div className="icon-holder">
+                  <img id="menu-svg" src={svgIcons} alt="" style={{left:-716, top:-248}} />
+                </div>
+              </div>
+
+              <div className="menu-option-div">
+                <div className="icon-holder">
+                  <img id="menu-svg" src={svgIcons} alt="" style={{left:-1354, top:-950}} />
+                </div>
+              </div>
+
+              <div className="menu-option-div">
+                <div className="icon-holder">
+                  <img id="menu-svg" src={svgIcons} alt="" style={{left:-1822, top:-981}} />
+                </div>
+              </div>
+
+              <div className="menu-option-div">
+                <div className="icon-holder">
+                  <img id="menu-svg" src={svgIcons} alt="" style={{left:-1216, top:-312}} />
+                </div>
+              </div>
+
+              <div className="menu-option-div" style={{gap:10}}>
+                <p>100%</p>
+                <MdArrowDropDown />
+              </div>
+
             </div>
+
+            <div className="divider"></div>
+
+            <div className="group2"></div>
           </div>
         </div>
 
         <div className="document-edit-section">
-          <svg  viewBox="8.18 18.22 40 25.26"  style={{backgroundColor:'pink', height:1224,width:1938}}>
-          <path fill="red" d="M400 902q-.825 0-1.412-.587Q398 900.825 398 900v-9l5-5h9q.825 0 1.413.588.587.587.587 1.412v4.5l4-4v11l-4-4v4.5q0 .825-.587 1.413-.588.587-1.413.587Zm0-2h12v-12h-8l-4 4Z"/>
-          </svg>
         </div>
       </div>
     </>

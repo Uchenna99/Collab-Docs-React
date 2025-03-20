461d36462f4b0122838c0edebaa3527eb2ac5d65
Uchenna99 <uchennaagbu@gmail.com>
2025-03-20 23:06:30 +0100

    building the editing canvas

--- src/pages/DocumentPage.tsx
+++ src/pages/DocumentPage.tsx
@@ -1,6 +1,5 @@
 import { LuHistory } from "react-icons/lu";
 import { MdOutlineComment } from "react-icons/md";
-import { BiVideo } from "react-icons/bi";
 import { MdArrowDropDown } from "react-icons/md";
 import { MdOutlineSearch } from "react-icons/md";
 import svgIcons from "../assets/material_common_sprite794_gm3_grey_medium.svg"
@@ -71,6 +70,12 @@ const DocumentPage = () => {
               <div className="share-div-arrow">
                 <MdArrowDropDown style={{color:'#414549'}} />
               </div>
+            </div>
+
+            <div className="circle-hover">
+              <svg focusable="false" viewBox="0 -960 960 960" height="28" width="28">
+                <path d="M480-80q2,0 2-2q0-82 31-154t85-126t126-85t154-31q2,0 2-2t-2-2q-82,0-154-31T598-598T513-724T482-878q0-2-2-2t-2,2q0,82-31,154T362-598T236-513T82-482q-2,0-2,2t2,2q82,0 154,31t126,85t85,126T478-82q0,2 2,2Z"></path>
+              </svg>
             </div>
 
             <div className="profile-img-div">
@@ -309,6 +314,11 @@ const DocumentPage = () => {
         </div>
 
         <div className="document-edit-section">
+          <div className="editing-section">
+            <div className="editing-sheets-div">
+              <div className="editing-canvas"></div>
+            </div>
+          </div>
         </div>
       </div>
     </>

9db4b1731880a571016d986f495b3ca344ecf986
Uchenna99 <uchennaagbu@gmail.com>
2025-03-22 00:39:12 +0100

    building the editing canvas

--- src/pages/DocumentPage.tsx
+++ src/pages/DocumentPage.tsx
@@ -343,8 +343,10 @@ const DocumentPage = () => {
 
               <div className="doc-tabs">
                 <p>Document tabs</p>
-                <div className="icon-holder">
-                  <img id="menu-svg" src={svgIcons} alt="" style={{left:-356, top:-422}} />
+                <div className="circle-hover" style={{width:30, height:30}}>
+                  <div className="icon-holder">
+                    <img id="menu-svg" src={svgIcons} alt="" style={{left:-356, top:-422}} />
+                  </div>
                 </div>
               </div>
             </div>
